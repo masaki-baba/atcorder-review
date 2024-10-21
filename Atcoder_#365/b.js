2e7d81f33fe2d5acba5d1ae691d5b24c7a471d49
masaki-baba <masakioutlook@gmail.com>
2024-10-21 23:05:30 +0900

    [ANS363]javascript-a

--- Atcoder_#365/b.js
+++ Atcoder_#365/b.js
@@ -1,14 +1,28 @@
-const input = require('fs').readFileSync('/dev/stdin', 'utf8').trim();
-const R = parseInt(input); // 現在のレートを数値として取得
+// 標準入力からデータを取得
+const input = require('fs').readFileSync('/dev/stdin', 'utf8').trim().split('\n');
 
-let requiredIncrease = 0;
+// 1行目のデータを取得 (N, T, P)
+const [N, T, P] = input[0].split(' ').map(Number);
 
-if (R <= 99) {
-  requiredIncrease = 100 - R;
-} else if (R <= 199) {
-  requiredIncrease = 200 - R;
-} else if (R <= 299) {
-  requiredIncrease = 300 - R;
+// 2行目の髪の長さデータを取得 (L_i)
+let L = input[1].split(' ').map(Number);
+
+// 初日に髪の長さが T 以上の人数をカウント
+let count = L.filter(length => length >= T).length;
+
+// すでに T 以上の人が P 人以上なら 0 を出力
+if (count >= P) {
+  console.log(0);
+} else {
+  // それ以外の場合、毎日髪の長さを増やしてシミュレーション
+  let days = 0;
+  while (count < P) {
+    days++;
+    // 1日経過で全員の髪の長さを1増やす
+    L = L.map(length => length + 1);
+    // T 以上の人数を再カウント
+    count = L.filter(length => length >= T).length;
+  }
+  // 結果として日数を出力
+  console.log(days);
 }
-
-console.log(requiredIncrease);
